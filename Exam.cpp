6156e2eb3c251978c9eaebb3b7952c8d15bcbb33
MatthewObirek <matthewobirek@gmail.com>
2023-01-25 16:09:18 -0800

    Added questions to exam, filled out both classes

--- Exam.cpp
+++ Exam.cpp
@@ -1,2 +1,86 @@
 #include "Exam.hpp"
 
+
+//* Question Methods
+//Constructors
+//Default
+Exam::Question::Question(std::string line)
+{
+
+}
+//Copy
+Exam::Question::Question(const Question& source)
+    :Q(source.Q), A(source.A), type(source.type), 
+     hint(source.hint) {}
+//Move
+Exam::Question::Question(Question&& source)
+    :Q(std::move(source.Q)), A(std::move(source.A)), 
+     type(source.type), hint(std::move(source.hint)) {}
+
+//Constructors
+Exam::Exam() :Exam(100) {}
+Exam::Exam(int capacity) : capacity(capacity)
+{
+    QuestionList = new std::vector<Question>;
+    QuestionList->reserve(capacity);
+
+    MCAnswerList = new std::vector<std::string>;
+    MCAnswerList->reserve(capacity);
+}
+
+//Deconstructor
+Exam::~Exam()
+{
+    //? Maybe write to file exam history??
+    delete QuestionList;
+    delete MCAnswerList;
+}
+
+//setters
+void Exam::setCapacity(int capacity)
+{
+    this->capacity = capacity;
+}
+
+//Builders
+void Exam::BuildFromFile(std::string filename)
+{
+    //! Create Array of Questions
+    //Needed Variables
+    std::ifstream file(filename);
+    std::vector<Question> Array;
+
+    if (file.is_open()) 
+    {
+        std::string line;
+        while(std::getline(file, line))
+        {
+            Question Q(line);
+            Array.push_back(Q);
+        }
+    }
+    else
+    {
+        std::cout << "Unable to open file: " << filename << std::endl;
+    }
+
+}
+
+void Exam::BuildFromList(std::vector<Question>& Array)
+{
+    if(Array.size()>capacity)
+    {
+        capacity = Array.size();
+        QuestionList->reserve(capacity);
+        MCAnswerList->reserve(capacity);
+    }
+
+    for (int i = 0; i < Array.size(); i++)
+    {
+        QuestionList->emplace_back(Array.at(i));
+        if(Array.at(i).type == Question::Type::MC)
+        {
+            MCAnswerList->emplace_back(Array.at(i).A);
+        }
+    }
+}
--- Exam.hpp
+++ Exam.hpp
@@ -1,14 +1,58 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 class Exam
 {
 private:
-
+    class Question
+    {
+    public:
+        enum Type {
+            MC, TF, SA, LA
+        };
+        std::string Q;
+        Type type;
+        std::string A;
+        std::string hint;
+        
+    public:
+        //Default Constructor
+        Question(std::string line);
+        //Copy Constructor
+        Question(const Question& source);
+        //Move Constructor
+        Question(Question&& source);
+        
+    private:
+        //Type Constructor;
+        Question(); //TF
+        Question(); //MC
+        Question(); //SA
+        Question(); //LA
+    public:
+        void toString();
+    };
+    int capacity;
+    std::vector<std::string>* MCAnswerList;
+    std::vector<Question>* QuestionList;
 public:
+    //Constructors
     Exam();
+    Exam(int capacity);
 
+    //getters
+    inline Question getQuestionAt(int i) { return QuestionList->at(i);}
+    inline int getCapacity() { return capacity; } 
+    inline int getSize() { return QuestionList->size();}
+    //setters
+    void setCapacity(int capacity);
 
-
+    //Builders
+    void BuildFromFile(std::string filename);
+    void BuildFromList(std::vector<Question>& Array);
 
 };
 
