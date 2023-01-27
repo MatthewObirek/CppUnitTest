aa97437290f2fa8f48b9ece1435167d3ebc8c36a
MatthewObirek <matthewobirek@gmail.com>
2023-01-26 16:48:50 -0800

    Updated readme

--- Main.cpp
+++ Main.cpp
@@ -4,10 +4,14 @@
 
 int main(int args, char** arg)
 {
+    //! List test folder
     std::cout << "you entered this File" <<arg[1] << std::endl;
     Exam exam;
-
+    //* Build exam
     exam.BuildFromFile(arg[1]);
-
+    
+    //! Run Test
+    //will print and allow answer for each test.
+    
     return 0;
 }
