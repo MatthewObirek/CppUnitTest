5b07bb453a3cd74b2ef698d2b407fc62b1f4f236
MatthewObirek <matthewobirek@gmail.com>
2023-02-07 12:10:39 -0800

    Added Default Test folder

--- Main.cpp
+++ Main.cpp
@@ -1,14 +1,41 @@
 #include <iostream>
+#include <filesystem>
 
 #include "Exam.hpp"
 
 int main(int args, char** arg)
 {
-    //! List test folder
-    std::cout << "you entered this File" <<arg[1] << std::endl;
+    std::string testName;
+    std::string Location = "tests";
+    std::vector<std::string> testlist;
+    //* checks for test
+    if (args > 1)
+    {
+        std::cout << "you entered this File" <<arg[1] << std::endl;
+        testName = arg[1];
+    }
+    else
+    {
+        //* lists test folder if there are no tests.
+        std::cout << "you entered No File" << std::endl;
+        std::cout << "Loading Test Folder at :"<< Location << std::endl;
+        int n = 1;
+        for (const auto & entry : std::filesystem::directory_iterator(Location))
+        {
+            testlist.push_back(entry.path().filename().string());
+            std::cout << "\t"<< n <<  ". " << testlist.back() << std::endl;
+            n++;   
+        }
+        n = 0;
+        std::cout << "Which test would you like to choose: ";
+        std::cin >> n;
+        testName = Location+"/"+testlist.at(n-1);
+        std::cout << testName << std::endl;
+    }
+    
     Exam exam;
     //* Build exam
-    exam.buildFromFile(arg[1]);
+    exam.buildFromFile(testName);
     
     //* Run Test
     //will print and allow answer for each test.
--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Before editing any code, or attempting to run, do these commands
 1. `mkdir out out/build out/arc out/obj`
 2. `cmake .`
 3. `make`
+
 **For testing, run the example test**
 - `./out/build/Main tests/ExampleQuiz.txt`
 
